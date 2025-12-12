c433dcd5f510ffe933c24810b26f6e47d19dc802
Johannes Z <17571653+johannesz-codes@users.noreply.github.com>
2025-12-12 13:47:26 +0100

    apply black formating to torcwa

--- torcwa/__init__.py
+++ torcwa/__init__.py
@@ -2,5 +2,5 @@ from .torch_eig import Eig as Eig
 from .geometry import geometry as geometry, rcwa_geo as rcwa_geo
 from .rcwa import rcwa as rcwa
 
-__author__ = '''Changhyun Kim'''
-__version__ = '0.1.4.2'
+__author__ = """Changhyun Kim"""
+__version__ = "0.1.4.2"
--- torcwa/torch_eig.py
+++ torcwa/torch_eig.py
@@ -1,15 +1,16 @@
 import torch
 
-'''
+"""
 Pytorch 1.10.1
 Complex domain eigen-decomposition with numerical stability
-'''
+"""
+
 
 class Eig(torch.autograd.Function):
     broadening_parameter = 1e-10
 
     @staticmethod
-    def forward(ctx,x):
+    def forward(ctx, x):
         ctx.input = x
         eigval, eigvec = torch.linalg.eig(x)
         ctx.eigval = eigval.cpu()
@@ -17,7 +18,7 @@ class Eig(torch.autograd.Function):
         return eigval, eigvec
 
     @staticmethod
-    def backward(ctx,grad_eigval,grad_eigvec):
+    def backward(ctx, grad_eigval, grad_eigvec):
         eigval = ctx.eigval.to(grad_eigval)
         eigvec = ctx.eigvec.to(grad_eigvec)
 
@@ -26,15 +27,17 @@ class Eig(torch.autograd.Function):
 
         # Lorentzian broadening: get small error but stabilizing the gradient calculation
         if Eig.broadening_parameter is not None:
-            F = torch.conj(s)/(torch.abs(s)**2 + Eig.broadening_parameter)
+            F = torch.conj(s) / (torch.abs(s) ** 2 + Eig.broadening_parameter)
         elif s.dtype == torch.complex64:
-            F = torch.conj(s)/(torch.abs(s)**2 + 1.4e-45)
+            F = torch.conj(s) / (torch.abs(s) ** 2 + 1.4e-45)
         elif s.dtype == torch.complex128:
-            F = torch.conj(s)/(torch.abs(s)**2 + 4.9e-324)
+            F = torch.conj(s) / (torch.abs(s) ** 2 + 4.9e-324)
 
-        diag_indices = torch.linspace(0,F.shape[-1]-1,F.shape[-1],dtype=torch.int64)
-        F[diag_indices,diag_indices] = 0.
-        XH = torch.transpose(torch.conj(eigvec),-2,-1)
+        diag_indices = torch.linspace(
+            0, F.shape[-1] - 1, F.shape[-1], dtype=torch.int64
+        )
+        F[diag_indices, diag_indices] = 0.0
+        XH = torch.transpose(torch.conj(eigvec), -2, -1)
         tmp = torch.conj(F) * torch.matmul(XH, grad_eigvec)
 
         grad = torch.matmul(torch.matmul(torch.inverse(XH), grad_eigval + tmp), XH)
