98338dc94ca7eb64cc09c7c116f21b270ef274ea
Johannes Z <17571653+johannesz-codes@users.noreply.github.com>
2025-12-17 16:03:20 +0100

    Update module structure

--- torcwa/__init__.py
+++ torcwa/__init__.py
@@ -1,7 +1,7 @@
 from .torch_eig import Eig as Eig
 from .geometry import geometry as geometry, rcwa_geo as rcwa_geo
 from .rcwa import rcwa as rcwa
-from .materials import Material as Material
+from . import materials
 
 __author__ = """Changhyun Kim"""
 __version__ = "0.1.4.2"
