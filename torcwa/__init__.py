2ec67f56c5dafa5b782052c03614d50cc2431631
Johannes Z <17571653+johannesz-codes@users.noreply.github.com>
2025-12-18 14:25:39 +0100

    Address lint issue: update init.py to proper api

--- torcwa/__init__.py
+++ torcwa/__init__.py
@@ -1,7 +1,7 @@
 from .torch_eig import Eig as Eig
 from .geometry import geometry as geometry, rcwa_geo as rcwa_geo
 from .rcwa import rcwa as rcwa
-from . import materials
+from . import materials as materials
 
 __author__ = """Changhyun Kim"""
 __version__ = "0.1.4.2"
